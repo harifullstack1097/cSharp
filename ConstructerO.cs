35aa9a0980cd51bdee4b744df422ff2c2af3b5de
Hari <harikrishnareddy1097@gmail.com>
2025-11-22 21:14:08 +0530

    inherit

--- ConstructerO.cs
+++ ConstructerO.cs
@@ -1,28 +1,27 @@
 ﻿using System;
 
-class Program
+class ConstructerO
     {
 
     string filename = "student.doc";
     string name = "Hari krishna reddy";
-    public Program(string sname) { 
+    public ConstructerO(string sname) { 
 
       Console.WriteLine(sname);
       this.name = sname;
     }
 
-    public Program(string sname, string id)
+    public ConstructerO(string sname, string id)
     {
 
         Console.WriteLine($"my name is {sname} and my id is {id}");
         this.name = sname;
     }
 
-
     static void Main(string[] args)
     {
-        Program k1 = new Program("student2");
-        Program k2 = new Program("Hari, 10");
+        ConstructerO k1 = new ConstructerO("student2");
+        ConstructerO k2 = new ConstructerO("Hari, 10");
         method();
         Console.WriteLine("hi this from main method");
     }
@@ -30,8 +29,6 @@ class Program
      {
         Console.WriteLine("hello");
      }
-
-
 
      }
     
---
+++ MethodOveriding.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+    public class MethodOveriding
+    {
+    public virtual void MethodParent() 
+        {
+            Console.WriteLine("Hi hello");
+        }
+    public void Show(string name)
+    {
+        Console.WriteLine($"i am {name}");
+    }
+
+    }
+class M1 : MethodOveriding
+{
+    public override void MethodParent()
+    {
+        Console.WriteLine("Iam from sencod class ");
+    }
+
+}
+class MethodOveriding1
+{
+    public void MethodParent() 
+    {
+     Console.WriteLine("This is from class 3");
+    }
+    static void Main() 
+    {
+        MethodOveriding m1 = new MethodOveriding();
+        MethodOveriding m2 = new M1();
+        m1.Show("hari");
+        m1.MethodParent(); 
+        m2.MethodParent();
+        MethodOveriding m3 = new MethodOveriding();
+        m3.MethodParent();
+    } 
+}
+
+
+
+
