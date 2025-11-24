556d497190a61239c4104fd28ec957e8aaa1f0cd
Hari <harikrishnareddy1097@gmail.com>
2025-11-24 21:27:26 +0530

    basic

---
+++ BaiscPratice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.CodeDom;
+
+public class BaiscPratice
+{
+
+    static void Main()
+    {
+        Console.WriteLine("THis is for loop");
+        int i = 5;
+        int age = 18;
+        for (i = 0; i < 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+
+        if (age >= 18)
+        {
+            Console.WriteLine("hi you are eligible to Vote");
+        }
+        else
+        {
+            Console.WriteLine("Sorry! you are not eligible to vote");
+        }
+
+        Console.WriteLine("This is While loop");
+        while (i <= 10) 
+        {
+            age = +i;
+            Console.WriteLine($"hi this while loop{age}");
+            i++;        
+        }
+       
+        
+    }
+}
+
+
+
+
---
+++ Overloading.cs
@@ -0,0 +1,39 @@
+﻿using System;
+    public class Overloading
+    {
+
+
+
+    public void Overloading1() 
+    {
+        Console.WriteLine("No parameter in this code");
+     }
+    public void Overloading1(string name)
+    {
+        Console.WriteLine($"Hi hell {name}");
+    }
+    public void Overloading1(string name, int age) 
+    {
+        Console.WriteLine($"{name} and age is {age}");
+    }
+
+
+    static void Main() 
+    {
+        Console.WriteLine("Hi hello");
+        Overloading overloading = new Overloading();
+
+        Console.WriteLine("calling no parameter method ");
+        overloading.Overloading1();
+
+        Console.WriteLine("calling single parameter method ");
+        overloading.Overloading1("Abishek");
+
+        Console.WriteLine("calling two parameter method ");
+        overloading.Overloading1("Raj",18);
+
+        Console.WriteLine("This is Method overloading");
+    }
+
+    }
+
